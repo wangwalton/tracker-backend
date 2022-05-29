4226496af31804052d671790b79cf3cf001d4bd0
Walton Wang <yuwaltonwang@gmail.com>
2022-05-29 05:57:12 -0400

    add docker

---
+++ init.sh
@@ -0,0 +1,17 @@
+# Ubuntu
+
+## Installing node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completio
+
+
+nvm install node
+npm install -g yar
+
+
+
+## Installing docker
+https://docs.docker.com/engine/install/ubuntu/
